c905ad9a9d514e975ef7265b3cd6b42e09729162
spaceisstrange <fgonzaleva@gmail.com>
2018-04-24 16:51:35 +0200

    Initial implementation of forms

--- lastfm2spotify/src/app/app.module.ts
+++ lastfm2spotify/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component'
 import { AppRoutingModule } from './/app-routing.module'
@@ -17,6 +18,7 @@ import { PlaylistCreatorComponent } from './playlist-creator/playlist-creator.co
     imports: [
         BrowserModule,
         AppRoutingModule,
+        FormsModule,
     ],
     providers: [LocalStorageService],
     bootstrap: [AppComponent]
--- lastfm2spotify/src/app/playlist-creator/playlist-creator.component.ts
+++ lastfm2spotify/src/app/playlist-creator/playlist-creator.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaylistCreatorComponent implements OnInit {
     /**
+     * Username of the user inputted in the text input.
+     */
+    lastfmUsername = ''
+
+    /**
+     * Name of the playlist in which the user wants to import the songs, inputted in the text input.
+     */
+    spotifyPlaylistName = ''
+
+    /**
      * The current selection from the list.
      */
     currentType = 'loved'
@@ -14,6 +24,14 @@ export class PlaylistCreatorComponent implements OnInit {
     constructor() { }
 
     ngOnInit() {
+
+    }
+
+    /**
+     * Called by the "Import it!" button. Takes the input from both of the text inputs, checks
+     * what import option was selected and starts the process.
+     */
+    importIntoPlaylist() {
 
     }
 }
